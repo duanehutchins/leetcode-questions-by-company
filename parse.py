879c2049e821adee437845abcbaab84554e91af8
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-05-29 20:17:50 -0500

    unittesting is finished

--- parse.py
+++ parse.py
@@ -1,29 +1,37 @@
+res: str = ""
+
 class Line:
     def __init__(self, line) -> None:
         self.__lst = [i.strip() for i in line.split('|') if i]
+        self.__title = get_title(line)
         self.__root = "https://leetcode.com/problems/"
         self.__route = self.__lst[1].replace(" ", "-").lower()
 
         for i in range(len(self.__lst)):
-            self.__lst[1] = self.__root + self.__route
+            self.__lst[1] = self.__root + self.__route + "/"
         
     def get_list(self):
         return self.__lst
+    
+    def get_formatted_line(self):
+        return f'| {self.__lst[0]} | [{self.__title}]({self.__lst[1]}) | {self.__lst[2]} | {self.__lst[3]} |\n'
 
-def list_to_string(lst):
-    return ''.join(lst[:6])
-        
+
+def get_title(line):
+    return [i.strip() for i in line.split('|') if i][1]
+
 def read_file(file_path):
     with open(file_path) as file:
+        global res
         res = ""
         data = file.readlines()
         res += ''.join(data[:6])
         data = data[6:]
         # print(data)
         for line in data:
-            title = [i.strip() for i in line.split('|') if i][1]
+            title = get_title(line)
             lineObj = Line(line).get_list()
-            res += f'| {lineObj[0]} | [{title}]({lineObj[1]}/) | {lineObj[2]} | {lineObj[3]} |\n'
+            res += f'| {lineObj[0]} | [{title}]({lineObj[1]}) | {lineObj[2]} | {lineObj[3]} |\n'
         file.close()
 
 
--- test_parse.py
+++ test_parse.py
@@ -1,11 +1,29 @@
 import unittest
 import parse
 
+line = "| 717 | 1-bit and 2-bit Characters | Easy | Google |"
+
 class TestParse(unittest.TestCase):
+
 
     def test_add(self):
         result = parse.add(1,2)
         self.assertEqual(3, result)
+    
+    def test_get_title(self):
+        result = '1-bit and 2-bit Characters'
+        self.assertEqual(parse.get_title(line), result)
+    
+    def test_get_list_with_updated_link(self):
+        result = ["717","https://leetcode.com/problems/1-bit-and-2-bit-characters/","Easy", "Google"]
+        lineObj = parse.Line(line=line).get_list()
+        self.assertEqual(result, lineObj)
+
+    def test_get_formatted_line(self):
+        result = "| 717 | [1-bit and 2-bit Characters](https://leetcode.com/problems/1-bit-and-2-bit-characters/) | Easy | Google |\n"
+        formatted_line = parse.Line(line).get_formatted_line()
+        self.assertEqual(result, formatted_line)
+
 
 
 
